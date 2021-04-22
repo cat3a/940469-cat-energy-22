c781d50e34743280a37eebc05fae94e568b2bf2d
cat3a <cat3a-88@mail.ru>
2021-04-22 23:15:53 +0300

    Исправляет часть ошибок. Добавляет иконки. Начинает работу с графикой

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,14 +3,7 @@ const navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--no-js');
 
-navToggle.addEventListener('click', function() {
-  /*if (navMain.classList.contains('main-nav--closed')) {
-  navMain.classList.remove('main-nav--closed');
-  navMain.classList.add('main-nav--opened');
-} else {
-  navMain.classList.add('main-nav--closed');
-  navMain.classList.remove('main-nav--opened');
-} */
+navToggle.addEventListener('click', function () {
   navMain.classList.toggle("main-nav--closed");
   navMain.classList.toggle("main-nav--opened");
 });
