ffd441e1f04df920be96fe7336f6ca9780d48b04
cat3a <cat3a-88@mail.ru>
2021-04-23 15:28:10 +0300

    Делает заготовку под слайдер

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,6 +2,8 @@ const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--no-js');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed');
 
 navToggle.addEventListener('click', function () {
   navMain.classList.toggle("main-nav--closed");
